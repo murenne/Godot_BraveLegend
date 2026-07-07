4bc9cd6c54678a448b48d65627ff29be79ff2c62
murenne <murenne@163.com>
2026-07-07 15:37:53 +0900

    添加了前景和背景

--- world.gd
+++ world.gd
@@ -1,16 +1,18 @@
 extends Node2D
 
-@onready var tile_map_layer: TileMapLayer = $TileMapLayer
-@onready var camera_2d: Camera2D = $TileMapLayer/Player/Camera2D
+@onready var geometry: TileMapLayer = $Node2D/Geometry
+@onready var camera_2d: Camera2D = $Player/Camera2D
 
 func _ready() -> void:
-	var used := tile_map_layer.get_used_rect()
-	var tile_size := tile_map_layer.tile_set.tile_size
 	
+	var used := geometry.get_used_rect().grow(-1) # 获得边界往里面一格.grow(-1)
+	var tile_size := geometry.tile_set.tile_size # 获得每个tile的尺寸
+	
+	# 设置四个角的最大值，position是左上，end是右下
 	camera_2d.limit_top = used.position.y * tile_size.y
 	camera_2d.limit_right = used.end.x * tile_size.x
 	camera_2d.limit_bottom = used.end.y * tile_size.y
 	camera_2d.limit_left = used.position.x * tile_size.x
 	
-	
+	# 防止最开始的时候相机移动会有延迟
 	camera_2d.reset_smoothing()

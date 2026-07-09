16f66ec5cd336618bf2323600a9d88f75dd7859f
murenne <murenne@163.com>
2026-07-09 11:49:28 +0900

    添加了滑墙逻辑

--- player.gd
+++ player.gd
@@ -7,6 +7,7 @@ enum State
 	JUMPING,
 	FALLING,
 	LANDING,
+	WALL_SLIDING,
 }
 
 const GROUND_STATES := [State.IDLE, State.RUNNING, State.LANDING]
@@ -18,17 +19,22 @@ const JUMP_VELOCITY := -350.0
 var default_gravity := ProjectSettings.get("physics/2d/default_gravity") as float
 var is_first_tick := false
 
-
-@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var graphics: Node2D = $Graphics
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var coyote_timer: Timer = $CoyoteTimer
 @onready var jump_request_timer: Timer = $JumpRequestTimer
+@onready var hand_checker: RayCast2D = $Graphics/HandChecker
+@onready var feet_checker: RayCast2D = $Graphics/FeetChecker
+
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump"):
 		jump_request_timer.start()
-	if event.is_action_released("jump") and velocity.y < JUMP_VELOCITY / 2:
-		velocity.y = JUMP_VELOCITY / 2
+	if event.is_action_released("jump"):
+		jump_request_timer.stop()
+		if velocity.y < JUMP_VELOCITY / 2:
+			velocity.y = JUMP_VELOCITY / 2
 
 func tick_physics(state: State, delta: float) -> void:
 	
@@ -47,6 +53,10 @@ func tick_physics(state: State, delta: float) -> void:
 			
 		State.LANDING:
 			stand(delta)
+			
+		State.WALL_SLIDING:
+			move(default_gravity /3, delta)
+			graphics.scale.x = get_wall_normal().x
 	
 	is_first_tick = false
 
@@ -58,7 +68,7 @@ func move(gravity: float, delta:float)->void:
 	velocity.y += gravity * delta
 	
 	if not is_zero_approx(direction):
-		sprite_2d.flip_h = direction < 0
+		graphics.scale.x = -1 if direction < 0 else 1
 	
 	move_and_slide()
 			
@@ -99,10 +109,20 @@ func get_next_state(state : State) -> State :
 		State.FALLING:
 			if is_on_floor():
 				return State.LANDING if is_still else State.RUNNING
+			if is_on_wall() and hand_checker.is_colliding() and feet_checker.is_colliding():
+				return State.WALL_SLIDING
 				
 		State.LANDING:
+			if not is_still:
+				return State.RUNNING
 			if not animation_player.is_playing():
 				return State.IDLE
+		
+		State.WALL_SLIDING:
+			if is_on_floor():
+				return State.IDLE
+			if not is_on_wall():
+				return State.FALLING
 
 	return state
 	
@@ -131,6 +151,8 @@ func transition_state(from: State,to: State) -> void:
 		State.LANDING:
 			animation_player.play("landing")
 			
-	
+		State.WALL_SLIDING:
+			animation_player.play("wall_sliding")
+		
 	is_first_tick = true
 	
